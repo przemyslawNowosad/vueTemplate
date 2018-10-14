2e3ce67dc6a96f8418dd7ef60f170d0771d3b3d3
Pszem <przemolec@gmail.com>
2018-10-14 21:29:20 +0200

    deploy

---
+++ precache-manifest.d30a55c5220361b3a680a95344955695.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueTemplate/robots.txt"
+  },
+  {
+    "revision": "b092590ca09e956cfb71",
+    "url": "/vueTemplate/js/chunk-vendors.7691f86a.js"
+  },
+  {
+    "revision": "49d11835dfaff9982710",
+    "url": "/vueTemplate/js/app.a994a972.js"
+  },
+  {
+    "revision": "7aea521844d961cc5415",
+    "url": "/vueTemplate/js/about.8aefc48a.js"
+  },
+  {
+    "revision": "f691708fb02611b282037796011ee9ba",
+    "url": "/vueTemplate/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vueTemplate/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/vueTemplate/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/vueTemplate/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "b092590ca09e956cfb71",
+    "url": "/vueTemplate/css/chunk-vendors.f31a5d75.css"
+  },
+  {
+    "revision": "49d11835dfaff9982710",
+    "url": "/vueTemplate/css/app.90c2e9e7.css"
+  }
+];
